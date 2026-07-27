2b1ffad62dca261871bba3630f8b7ba47c46a01c
Aaron Oquendo <iaaron94@gmail.com>
2026-07-26 20:52:41 -0400

    Add 3Sum solution

---
+++ two-pointers/three-sum.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const result = [];
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        // Skip duplicate starting numbers.
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+
+                left++;
+                right--;
+
+                // Skip duplicate left values.
+                while (left < right && nums[left] === nums[left - 1]) {
+                    left++;
+                }
+
+                // Skip duplicate right values.
+                while (left < right && nums[right] === nums[right + 1]) {
+                    right--;
+                }
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+};
